f40ab612f94eceafd68d6c4c8d59731c35adc057
Rodrigo Batini <rodrigobatini@gmail.com>
2024-02-27 18:03:54 -0300

    crud and model for books

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_203942) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_210235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
